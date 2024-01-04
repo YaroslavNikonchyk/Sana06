bb5d6c9db76620ea6c6c81a6638bbea11123c58b
nikocukaroslav <nikocukaroslav2006@gmail.com>
2024-01-04 22:23:33 +0200

    created class people and inherited class applicant

---
+++ People-classes/People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People_classes
+{
+     internal class People
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public People(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public People()
+        {
+            FirstName = "David";
+            LastName = "Paklin";
+            BirthDate = new DateTime(2005, 9, 16);
+        }
+    }
+}
