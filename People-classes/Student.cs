6df675169d7d0e30ee3a7cda0eaea5acef569fd0
nikocukaroslav <nikocukaroslav2006@gmail.com>
2024-01-04 22:49:47 +0200

    created class student and rebuilded previous classes

---
+++ People-classes/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People_classes
+{
+    internal class Student : People
+    {
+        public int Course { get; set; }
+        public string Group { get; set; }
+        public string Faculty { get; set; }
+        public string HEI { get; set; }
+
+        public Student(string firstName, string lastName, DateTime birthDate, int course, string group, string faculty, string hEI)
+            : base(firstName, lastName, birthDate)
+
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            HEI = hEI;
+        }
+
+        public Student() { }
+
+    }
+}
