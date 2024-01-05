791e0143bd7be72ae76c8a0b95745a1fabc1e1af
nikocukaroslav <nikocukaroslav2006@gmail.com>
2024-01-05 10:58:34 +0200

    added some random birth time

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿using PeopleClasses;
 
 People[] people = new People[4];
-people[0] = new People("Yaroslav", "Nikonchuk", new DateTime(2006, 4, 19));
-people[1] = new Applicant("David", "Paklin", new DateTime(2005, 9, 16), 150, 10, "ZDSU");
-people[2] = new Student("Bodan", "Bogdanovich", new DateTime(2004, 4, 25), 1, 5, "IPZ", "ZDSU");
-people[3] = new Teacher("Andiy", "Morozov", new DateTime(1990, 1, 1), "rector", "IPZ", "ZDSU");
+people[0] = new People("Yaroslav", "Nikonchuk", new DateTime(2006, 4, 19,16,30,00));
+people[1] = new Applicant("David", "Paklin", new DateTime(2005, 9, 16, 18, 00, 00), 150, 10, "ZDSU");
+people[2] = new Student("Bodan", "Bogdanovich", new DateTime(2004, 4, 25, 11, 20, 00), 1, 5, "IPZ", "ZDSU");
+people[3] = new Teacher("Andiy", "Morozov", new DateTime(1990, 1, 1, 00, 00, 00), "rector", "IPZ", "ZDSU");
 
 for (int i = 0; i < people.Length; i++)
 {
