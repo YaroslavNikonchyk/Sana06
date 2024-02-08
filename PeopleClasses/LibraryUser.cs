0a370e1815268f2d57bb3766dd1b8909e85123fb
nikocukaroslav <nikocukaroslav2006@gmail.com>
2024-02-08 19:38:06 +0200

    fixed problems

---
+++ PeopleClasses/LibraryUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PeopleClasses
+{
+    public class LibraryUser : People
+    {
+        public int UserCardNumber { set; get; }
+        public DateTime IssueDate { set; get; }
+        public int ReaderFee { set; get; }
+
+        public LibraryUser(string firstName, string lastName, DateTime birthDate, int userCardNumber, DateTime issueDate, int readerFee)
+            : base(firstName, lastName, birthDate)
+        {
+            UserCardNumber = userCardNumber;
+            IssueDate = issueDate;
+            ReaderFee = readerFee;
+        }
+        public LibraryUser()
+        {
+            UserCardNumber = 123213;
+            IssueDate = new DateTime(2024, 3, 21);
+            ReaderFee = 100;
+        }
+
+        public override string ShowInfo()
+        {
+            return $"Name:{FirstName}; Lastname:{LastName}; BirthDate:{BirthDate}; UserCardNumber : {UserCardNumber}; " +
+                $"IssueDate: {IssueDate}; ReaderFee:{ReaderFee};";
+        }
+    }
+}
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using PeopleClasses;
 
-People[] people = new People[4];
-people[0] = new People("Yaroslav", "Nikonchuk", new DateTime(2006, 4, 19,16,30,00));
+People[] people = new People[5];
+people[0] = new People("Yaroslav", "Nikonchuk", new DateTime(2006, 4, 19, 16, 30, 00));
 people[1] = new Applicant("David", "Paklin", new DateTime(2005, 9, 16, 18, 00, 00), 150, 10, "ZDSU");
 people[2] = new Student("Bodan", "Bogdanovich", new DateTime(2004, 4, 25, 11, 20, 00), 1, 5, "IPZ", "ZDSU");
 people[3] = new Teacher("Andiy", "Morozov", new DateTime(1990, 1, 1, 00, 00, 00), "rector", "IPZ", "ZDSU");
+people[4] = new LibraryUser("Bogdan", "Gordon", new DateTime(2003, 2, 22), 23213, new DateTime(2024, 3, 21), 10);
 
 for (int i = 0; i < people.Length; i++)
 {
